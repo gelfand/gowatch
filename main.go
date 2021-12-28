6abb93440432767b6151c0b6e38603bf05cafc18
eugene <eugene@linux.com>
2021-12-28 20:22:11 +0200

    seems like it actually works

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -13,14 +12,6 @@ import (
 	"strings"
 	"time"
 )
-
-// errEmptyCmd returned when caller passes empty command.
-var errEmptyCmd = errors.New("command is empty")
-
-// NOTE: flock is not used, because it's all sequantial.
-//
-// flock is a global file lock.
-// var flock = sync.Mutex{}
 
 // Flags.
 var (
@@ -34,61 +25,49 @@ func usage() {
 	flag.PrintDefaults()
 }
 
-// cmdArgs is struct with command name and it's arguments.
-type cmdArgs struct {
-	name string
-	args []string
-}
-
-// config is the configuration for the gowatch.
-type config struct {
-	// Path is the path to watch.
-	Path string
-	args cmdArgs
-}
-
-// initConfig initializes the configurations.
-func initConfig() (config, error) {
-	entryPath, err := filepath.Abs(*pathFlag)
-	if err != nil {
-		return config{}, fmt.Errorf("invlaid path: %w", err)
-	}
-
-	cmdRaw := strings.Split(*cmdFlag, " ")
-	if len(cmdRaw) == 0 {
-		return config{}, errEmptyCmd
-	}
-
-	cmdName := cmdRaw[0]
-	var flags []string
-	if len(cmdRaw) > 1 {
-		flags = cmdRaw[1:]
-	}
-
-	return config{
-		Path: entryPath,
-		args: cmdArgs{
-			name: cmdName,
-			args: flags,
-		},
-	}, nil
-}
-
+// watcher watches the given path for changes.
 func watcher(ctx context.Context, dirpath string, notify chan<- struct{}) {
 	cycle := time.NewTicker(time.Second)
 	defer cycle.Stop()
 
 	fileInfos := make(map[string]os.FileInfo)
-	err := filepath.WalkDir(dirpath, func(path string, d os.DirEntry, err error) error {
-		fileInfo, err := d.Info()
+	var walk func(path string)
+	walk = func(path string) {
+		dirs, err := os.ReadDir(path)
 		if err != nil {
-			return fmt.Errorf("could not get %v info: %w", path, err)
+			// TODO: maybe ignore this
+			log.Fatalf("could start gowatch, could not read directory: %v", err)
 		}
-		fileInfos[path] = fileInfo
-		return nil
-	})
-	if err != nil {
-		log.Fatalf("could not start gowatch, after unsuccessful program initialization: %v", err)
+
+		for _, d1 := range dirs {
+			if strings.HasPrefix(d1.Name(), ".") {
+				continue
+			}
+
+			path1 := filepath.Join(path, d1.Name())
+			fileInfo, err := d1.Info()
+			if err != nil {
+				log.Fatal(err)
+			}
+			v, ok := fileInfos[path1]
+			if !ok {
+				go func() {
+					notify <- struct{}{}
+				}()
+				fileInfos[path1] = fileInfo
+			} else {
+				if v.Size() != fileInfo.Size() || v.ModTime() != fileInfo.ModTime() {
+					go func() {
+						notify <- struct{}{}
+					}()
+					fileInfos[path1] = fileInfo
+				}
+			}
+
+			if d1.IsDir() {
+				walk(path1)
+			}
+		}
 	}
 
 	for {
@@ -96,44 +75,21 @@ func watcher(ctx context.Context, dirpath string, notify chan<- struct{}) {
 		case <-ctx.Done():
 			return
 		case <-cycle.C:
-			changed := false
-			// TODO:: replace by own implementation to not call call recursively walk dir,
-			// because right not it's pretty junky implementation.
-			// See: path/filepath/path.go walkDir
-			filepath.WalkDir(dirpath, func(path string, d os.DirEntry, err error) error {
-				fileInfo, err := d.Info()
-				if err != nil {
-					return err
-				}
-				v, ok := fileInfos[path]
-				if !ok && !changed {
-					changed = true
-				}
-				if !changed && ((v.ModTime() != fileInfo.ModTime()) || (v.Size() != fileInfo.Size())) {
-					changed = true
-				}
-
-				fileInfos[path] = fileInfo
-				return nil
-			})
-
-			if changed {
-				go func() {
-					notify <- struct{}{}
-				}()
-			}
+			walk(dirpath)
 		}
 	}
 }
 
-func runner(ctx context.Context, args cmdArgs, notify <-chan struct{}) {
+// runner runs the given command.
+func runner(ctx context.Context, cmdName string, args []string, notify chan struct{}) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case <-notify:
+			log.Printf("\nReloading...\n")
 			cmdCtx, cancel := context.WithCancel(context.Background())
-			cmd := exec.CommandContext(cmdCtx, args.name, args.args...)
+			cmd := exec.CommandContext(cmdCtx, cmdName, args...)
 			go func() {
 				cmd.Run()
 			}()
@@ -143,7 +99,7 @@ func runner(ctx context.Context, args cmdArgs, notify <-chan struct{}) {
 				cancel()
 				return
 			case <-notify:
-				log.Printf("\nReloading...\n")
+				go func() { notify <- struct{}{} }()
 				cancel()
 			}
 		}
@@ -157,13 +113,22 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
-	cfg, err := initConfig()
+	path, err := filepath.Abs(*pathFlag)
 	if err != nil {
-		log.Fatalf("could not start gowatch: %v", err)
+		log.Fatalf("could not retrieve given path: %v", err)
+	}
+	cmdRaw := strings.Split(*cmdFlag, " ")
+	if len(cmdRaw) == 0 {
+		log.Fatalf("could not start gowatch, command is empty")
+	}
+	cmdName := cmdRaw[0]
+	var args []string
+	if len(cmdRaw) > 1 {
+		args = cmdRaw[1:]
 	}
 
 	notify := make(chan struct{}, 1)
 
-	go watcher(ctx, cfg.Path, notify)
-	runner(ctx, cfg.args, notify)
+	go watcher(ctx, path, notify)
+	runner(ctx, cmdName, args, notify)
 }
